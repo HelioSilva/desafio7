9a75239edf67ee30318d658052557f4702a42d6f
Helio Silva <suporte.computek@gmail.com>
2020-09-14 22:58:12 -0300

    Finished

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -18,6 +18,7 @@ export const Container = styled.div<ContainerProps>`
 
     nav {
       a {
+        cursor: pointer;
         color: #fff;
         text-decoration: none;
         font-size: 16px;
